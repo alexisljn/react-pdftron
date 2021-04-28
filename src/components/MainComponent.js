52299f727eaa5316081d907c63ac770c23134e80
alexisljn <alexis.lejeune@gmx.fr>
2021-04-28 17:27:08 +0200

    Add 2 Fixtures Files

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useRef} from 'react';
+import WebViewer from "@pdftron/webviewer";
 
 const MainComponent = () => {
+
+
+    const viewer = useRef(null);
+
+    useEffect(() => {
+        WebViewer(
+            {
+                path: '/pdftron/lib',
+                initialDoc: '/fixtures/sample.pdf',
+            },
+            viewer.current,
+        ).then((instance) => {
+            const { docViewer } = instance;
+            // you can now call WebViewer APIs here...
+        });
+    }, [])
+
+    return (
+        <div className="MyComponent">
+            <div className="header">React sample</div>
+            <div className="webviewer" ref={viewer} style={{height: "100vh"}}></div>
+        </div>
+    );
 
     return (
         <h1>From React</h1>
