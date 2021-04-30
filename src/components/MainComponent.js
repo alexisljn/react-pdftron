d41dab8b3b1b6af6a2132277755a0a40db79ccf5
alexisljn <alexis.lejeune@gmx.fr>
2021-04-30 16:03:31 +0200

    Go debug on another branch

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,13 +9,23 @@ const MainComponent = () => {
     const [webViewer, setWebViewer] = useState(null);
     const viewer = useRef(null);
 
-    const [target, setTarget] = React.useState();
+    const [targets, setTargets] = useState([]);
+    const [signatureCounter, setSignatureCounter] = useState(1);
+    const [emailCounter, setEmailCounter] = useState(1);
+    const [nameCounter, setNameCounter] = useState(1)
+
+    // const [target, setTarget] = React.useState();
     const [frame, setFrame] = React.useState({
         translate: [0,0],
     });
 
+    const SIGNATURE_TYPE = 'signature';
+    const EMAIL_TYPE = 'email';
+    const NAME_TYPE = 'name';
+
     useEffect(() => {
-        setTarget(document.querySelector(".target"));
+        // setTarget(document.querySelector(".target"));
+
         const loadWebViewer = async () => {
             const instance = await WebViewer(
                 {
@@ -28,11 +38,59 @@ const MainComponent = () => {
             setWebViewer(instance)
             handleClick(instance)
         }
-        (async () => {
-           await loadWebViewer()
 
-            handleClick()
-        })()
+
+        console.log("SIG COUNTER", signatureCounter);
+
+       // initiate
+       //  const signatureDOMTarget = createDOMTarget('signature', signatureCounter, '.sidebar');
+       //  const nameDOMTarget = createDOMTarget('name', nameCounter, '.sidebar');
+       //  const emailDOMTarget = createDOMTarget('email', emailCounter, '.sidebar');
+
+
+        setTargets([
+            {
+                // domElt: document.querySelector(`.target-signature-${signatureCounter}`),
+                domElt: `target-signature-${signatureCounter}`,
+                type: 'signature',
+                isActive: false,
+                id: signatureCounter,
+            },
+            {
+                // domElt: document.querySelector(`.target-name-${nameCounter}`),
+                domElt: `target-name-${nameCounter}`,
+                type: 'name',
+                isActive: false,
+                id: nameCounter,
+            },
+            {
+                // domElt: document.querySelector(`.target-email-${emailCounter}`),
+                domElt: `target-email-${emailCounter}`,
+                type: 'email',
+                isActive: false,
+                id: emailCounter,
+            }
+        ])
+        // addTargets({
+        //     domElt: document.querySelector(".target-signature-1"),
+        //     type: 'signature'
+        // })
+        // addTargets({
+        //     domElt: document.querySelector(".target-name-1"),
+        //     type: 'name'
+        // })
+        // addTargets({
+        //     domElt: document.querySelector(".target-email-1"),
+        //     type: 'email'
+        // })
+
+        console.log('TARGETS', targets);
+
+        // (async () => {
+        //    await loadWebViewer()
+        //
+        //     handleClick()
+        // })()
         // loadWebViewer();
 
 
@@ -59,8 +117,69 @@ const MainComponent = () => {
         })
     }
 
-    const onDragEnd = () => {
+    const incrementCounter = (type) => {
+        switch (type) {
+            case SIGNATURE_TYPE:
+                setSignatureCounter(() => signatureCounter + 1);
+                break;
+            case EMAIL_TYPE:
+                break;
+            case NAME_TYPE:
+                break;
+            default:
+                return;
+        }
+    }
 
+    const createDOMTarget = (type, counter, container) => {
+
+        const colorMapping =  {
+            signature: 'blue',
+            name: 'red',
+            email: 'green'
+        }
+        const containerElt = document.querySelector(container)
+        const targetClassname = `target-${type}-${counter}`
+        const targetElt = document.createElement('div');
+
+        targetElt.classList.add(targetClassname);
+        targetElt.textContent = type.toUpperCase();
+        targetElt.style.width = "150px";
+        targetElt.style.height = "30px";
+        targetElt.style.backgroundColor = colorMapping[type];
+        targetElt.style.color = "white";
+        targetElt.style.cursor = "pointer";
+        containerElt.appendChild(targetElt)
+
+        return targetClassname;
+    }
+
+
+    // const getDefaultTargets = () => {
+    //     return [
+    //         {
+    //             domElt: document.querySelector(".target-signature-1"),
+    //             type: 'signature',
+    //             isActive: false,
+    //             id: 1,
+    //         },
+    //         {
+    //             domElt: document.querySelector(".target-name-1"),
+    //             type: 'name',
+    //             isActive: false,
+    //             id: 2,
+    //         },
+    //         {
+    //             domElt: document.querySelector(".target-email-1"),
+    //             type: 'email',
+    //             isActive: false,
+    //             id: 3,
+    //         }]
+    // }
+
+    const addTarget = (target) => {
+        targets.push(target)
+        setTargets(targets);
     }
 
     const getStyle = (inputName) => {
@@ -79,187 +198,136 @@ const MainComponent = () => {
         }
     }
 
-    const createMoveable = (inputName) => {
-        console.log("CREATE MOVEABLE", inputName);
+    const createMoveable = (target) => {
+        // console.log("CREATE MOVEABLE", target);
+        // target.isActive = true;
+
+        //TODO Not sure if it's needed
+        // target['toto'] = 'toto';
+        const targetsCopy = JSON.parse(JSON.stringify(targets));
+        targetsCopy.forEach((targetFromState, index) => {
+            if (targetFromState.id === target.id && targetFromState.type === target.type)
+                targetFromState['isActive'] = true;
+        })
+        console.log('targCopy', targetsCopy);
+        console.log('targs', targets);
+        // setTargets(targets);
+
+        // console.log("targets with update", targets)
+        // Créér un movable = créer un élément dans le dom, (une nouvelle div avec la classe target) et un objet target
+        // dans le state
+       //  const sidebarElt = document.querySelector('.sidebar-signature');
+       //  const newDivElt = document.createElement('div')
+       //  newDivElt.classList.add('target-signature-2');
+       //  newDivElt.textContent = 'SIGNATURE';
+       //  newDivElt.style.width = "150px";
+       //  newDivElt.style.height = "30px";
+       //  newDivElt.style.backgroundColor = "blue";
+       //  newDivElt.style.color = "white";
+       //  newDivElt.style.cursor = "pointer";
+       //  sidebarElt.appendChild(newDivElt);
+       //
+       //  const newTarget = {
+       //      domElt: document.querySelector(".target-signature-2"),
+       //      type: 'signature',
+       //      isActive: false,
+       //      id: 4,
+       //  }
+       //
+       // targetsCopy.push(newTarget);
+       //  console.log("targets pre set", targets)
+        setTargets(targetsCopy);
+
     }
 
     const test = () => {
-        const {docViewer} = webViewer;
+        // const {docViewer} = webViewer;
+        //
+        // console.log("docViewerr", docViewer);
 
-        console.log("docViewerr", docViewer);
+        // addTarget({domElt:"toto",type:'toto'})
+        const newTarget = {
+            // domElt: document.querySelector(".target-signature-2"),
+            type: 'signatursse',
+            isActive: false,
+            id: 4,
+        }
+         //targets.push(newTarget);
+        setTargets((targets) => [...targets, newTarget]);
+        console.log("les targets", targets)
     }
 
     return (
         <>
             <button onClick={() => {test()}}>ZZ</button>
-            <div style={{height: "100vh", backgroundColor: "#E8E8E8", width: "25%"}}>
+            <div className="sidebar" style={{height: "100vh", backgroundColor: "#E8E8E8", width: "25%"}}>
                 SIDEBAR
-                <div style={getStyle('signature')}
-                     onClick={() => createMoveable('signature')}
-                >
-                    SIGNATURE
-                </div>
-                <div style={{marginBottom: 20}}/>
-                <div style={getStyle('name')}
-                     onClick={() => createMoveable('name')}
-                >
-                    NOM
-                </div>
-                <div style={{marginBottom: 20}}/>
-                <div style={getStyle('email')}
-                     onClick={() => createMoveable('email')}
-                >
-                    EMAIL
-                </div>
-                <div style={{marginBottom: 20}}/>
-                <div className="target" style={{width: 150, height: 50, backgroundColor: 'blue', position: 'absolute'}}>DRAGGABLE</div>
-                <Moveable
-                    target={document.querySelector(".target")}
-                    container={null}
-                    origin={true}
+                {/*Soit on récupere la position initial de chaque target */}
+                {/*Soit on créer une div wrapper pour chaque type de target et quand on récréé une target*/}
+                {/*on l'ajoute en enfant de cette div wrapper.*/}
+                {/*<div className="sidebar-signature">*/}
+                <div className={`target-signature-${signatureCounter}`} style={getStyle('signature')}>SIGNATURE</div>
+                {/*/!*<div style={{marginBottom: 20}} />*!/*/}
+                {/*</div>*/}
+                <div className={`target-name-${nameCounter}`} style={getStyle('name')}>NOM</div>
+                {/*/!*<div style={{marginBottom: 20}}/>*!/*/}
+                <div className={`target-email-${emailCounter}`} style={getStyle('email')}>EMAIL</div>
+                {/*/!*<div style={{marginBottom: 20}}/>*!/*/}
+                {/*{targets.map(target => (*/}
+                {/*    <div className={`target-${target.type}-${target.id}`}*/}
+                {/*         style={getStyle(target.type)}*/}
+                {/*    >*/}
+                {/*        {target.type.toUpperCase()}*/}
+                {/*    </div>*/}
+                {/*    )*/}
+                {/*)}*/}
 
-                    /* Resize event edges */
-                    edge={false}
-
-                    /* draggable */
-                    draggable={true}
-                    throttleDrag={0}
-                    onDragStart={({ target, clientX, clientY }) => {
-                        console.log("onDragStart", target);
-                    }}
-                    onDrag={({
-                                 target,
-                                 beforeDelta, beforeDist,
-                                 left, top,
-                                 right, bottom,
-                                 delta, dist,
-                                 transform,
-                                 clientX, clientY,
-                             }) => {
-                        // console.log("onDrag left, top", left, top);
-                        // target!.style.left = `${left}px`;
-                        // target!.style.top = `${top}px`;
-                        // console.log("onDrag translate", dist);
-                        target.style.transform = transform;
-                    }}
-                    onDragEnd={({ target, isDrag, clientX, clientY }) => {
-                        console.log("onDragEnd", target, isDrag);
-                    }}
-
-                    /* When resize or scale, keeps a ratio of the width, height. */
-                    keepRatio={true}
-
-                    /* resizable*/
-                    /* Only one of resizable, scalable, warpable can be used. */
-                    resizable={true}
-                    throttleResize={0}
-                    onResizeStart={({ target , clientX, clientY}) => {
-                        console.log("onResizeStart", target);
-                    }}
-                    onResize={({
-                                   target, width, height,
-                                   dist, delta, direction,
-                                   clientX, clientY,
-                               }) => {
-                        console.log("onResize", target);
-                        delta[0] && (target.style.width = `${width}px`);
-                        delta[1] && (target.style.height = `${height}px`);
-                    }}
-                    onResizeEnd={({ target, isDrag, clientX, clientY }) => {
-                        console.log("onResizeEnd", target, isDrag);
-                    }}
-
-                    /* scalable */
-                    /* Only one of resizable, scalable, warpable can be used. */
-                    scalable={true}
-                    throttleScale={0}
-                    onScaleStart={({ target, clientX, clientY }) => {
-                        console.log("onScaleStart", target);
-                    }}
-                    onScale={({
-                                  target, scale, dist, delta, transform,
-                                  clientX, clientY,
-                              }) => {
-                        console.log("onScale scale", scale);
-                        target.style.transform = transform;
-                    }}
-                    onScaleEnd={({ target, isDrag, clientX, clientY }) => {
-                        console.log("onScaleEnd", target, isDrag);
-                    }}
-
-                    /* rotatable */
-                    rotatable={true}
-                    throttleRotate={0}
-                    onRotateStart={({ target, clientX, clientY }) => {
-                        console.log("onRotateStart", target);
-                    }}
-                    onRotate={({
-                                   target,
-                                   delta, dist,
-                                   transform,
-                                   clientX, clientY,
-                               }) => {
-                        console.log("onRotate", dist);
-                        target.style.transform = transform;
-                    }}
-                    onRotateEnd={({ target, isDrag, clientX, clientY }) => {
-                        console.log("onRotateEnd", target, isDrag);
-                    }}
-
-                    /* warpable */
-                    /* Only one of resizable, scalable, warpable can be used. */
-                    /*
-                    this.matrix = [
-                        1, 0, 0, 0,
-                        0, 1, 0, 0,
-                        0, 0, 1, 0,
-                        0, 0, 0, 1,
-                    ]
-                    */
-                    warpable={true}
-                    onWarpStart={({ target, clientX, clientY }) => {
-                        console.log("onWarpStart", target);
-                    }}
-                    onWarp={({
-                                 target,
-                                 clientX,
-                                 clientY,
-                                 delta,
-                                 dist,
-                                 multiply,
-                                 transform,
-                             }) => {
-                        console.log("onWarp", target);
-                        // target.style.transform = transform;
-                        this.matrix = multiply(this.matrix, delta);
-                        target.style.transform = `matrix3d(${this.matrix.join(",")})`;
-                    }}
-                    onWarpEnd={({ target, isDrag, clientX, clientY }) => {
-                        console.log("onWarpEnd", target, isDrag);
-                    }}
-
-                    // Enabling pinchable lets you use events that
-                    // can be used in draggable, resizable, scalable, and rotateable.
-                    pinchable={true}
-                    onPinchStart={({ target, clientX, clientY, datas }) => {
-                        // pinchStart event occur before dragStart, rotateStart, scaleStart, resizeStart
-                        console.log("onPinchStart");
-                    }}
-                    onPinch={({ target, clientX, clientY, datas }) => {
-                        // pinch event occur before drag, rotate, scale, resize
-                        console.log("onPinch");
-                    }}
-                    onPinchEnd={({ isDrag, target, clientX, clientY, datas }) => {
-                        // pinchEnd event occur before dragEnd, rotateEnd, scaleEnd, resizeEnd
-                        console.log("onPinchEnd");
-                    }}
-                />
+                {/*<div className="target" style={{width: 150, height: 50, backgroundColor: 'blue', position: 'absolute'}}>DRAGGABLE</div>*/}
             </div>
         <div className="MyComponent" style={{width: '75%'}}>
             {/*<button onClick={() => console.log(webViewer)}>X</button>*/}
             {/*<div className="header">React sample</div>*/}
             <div className="webviewer" ref={viewer} style={{height: "100vh"}}></div>
         </div>
+            {targets.map(targetObj => {
+                console.log(`DOM.target-${targetObj.type}-${targetObj.id}`, document.querySelector(`.target-${targetObj.type}-${targetObj.id}`))
+                return (
+                    <>
+                    <p>{targetObj.type}</p>
+                    <Moveable
+                        target={document.querySelector(`.target-${targetObj.type}-${targetObj.id}`)}
+                        container={null}
+                        origin={false}
+                        edge={false}
+                        draggable={true}
+                        throttleDrag={0}
+                        onDragStart={({ target, clientX, clientY }) => {
+                            console.log("onDragStart", target);
+                        }}
+                        onDrag={({
+                                     target,
+                                     beforeDelta, beforeDist,
+                                     left, top,
+                                     right, bottom,
+                                     delta, dist,
+                                     transform,
+                                     clientX, clientY,
+                                 }) => {
+                            // console.log("onDrag left, top", left, top);
+                            // target!.style.left = `${left}px`; COM BY TUTO
+                            // target!.style.top = `${top}px`; COM BY TUTO
+                            // console.log("onDrag translate", dist);
+                            target.style.transform = transform;
+                            console.log("targetOBJ", targetObj)
+                            if (!targetObj.isActive) createMoveable(targetObj)
+                        }}
+                        onDragEnd={({ target, isDrag, clientX, clientY }) => {
+                            // console.log("onDragEnd", target, isDrag);
+                        }}
+                    />
+                    </>
+                )
+            })}
         </>
     );
 }
