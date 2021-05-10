83edf44fdf6ca0ec92e20ed93abb0e746718e0d1
alexisljn <alexis.lejeune@gmx.fr>
2021-05-10 11:59:35 +0200

    Add container for each input type

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -47,28 +47,28 @@ const MainComponent = () => {
        //  const nameDOMTarget = createDOMTarget('name', nameCounter, '.sidebar');
        //  const emailDOMTarget = createDOMTarget('email', emailCounter, '.sidebar');
 
-
+        // SetTargets pour pouvoir créer les premiers Moveable dans le DOM.
         setTargets([
             {
                 // domElt: document.querySelector(`.target-signature-${signatureCounter}`),
                 domElt: `target-signature-${signatureCounter}`,
                 type: 'signature',
                 isActive: false,
-                id: signatureCounter,
+                id:  1,
             },
             {
                 // domElt: document.querySelector(`.target-name-${nameCounter}`),
                 domElt: `target-name-${nameCounter}`,
                 type: 'name',
                 isActive: false,
-                id: nameCounter,
+                id: 1,
             },
             {
                 // domElt: document.querySelector(`.target-email-${emailCounter}`),
                 domElt: `target-email-${emailCounter}`,
                 type: 'email',
                 isActive: false,
-                id: emailCounter,
+                id: 1,
             }
         ])
         // addTargets({
@@ -83,15 +83,12 @@ const MainComponent = () => {
         //     domElt: document.querySelector(".target-email-1"),
         //     type: 'email'
         // })
-
         // (async () => {
         //    await loadWebViewer()
         //
         //     handleClick()
         // })()
         // loadWebViewer();
-
-
         // WebViewer(
         //     {
         //         licenseKey: 'toto',
@@ -116,29 +113,45 @@ const MainComponent = () => {
     }
 
     const incrementCounter = (type) => {
+        let newValue;
         switch (type) {
             case SIGNATURE_TYPE:
-                setSignatureCounter((signatureCounter) => signatureCounter + 1);
+                console.log('temoin signature');
+                newValue = signatureCounter + 1;
+                setSignatureCounter(newValue);
                 break;
             case EMAIL_TYPE:
-                setEmailCounter((emailCounter) => emailCounter + 1);
+                console.log('temoin email');
+                newValue = emailCounter + 1
+                setEmailCounter(newValue);
                 break;
             case NAME_TYPE:
-                setNameCounter((nameCounter) => nameCounter + 1);
+                console.log('temoin name');
+                newValue = nameCounter + 1
+                setNameCounter(newValue);
                 break;
             default:
                 return;
         }
+
+        return newValue
     }
 
-    const createDOMTarget = (type, counter, container) => {
+    const createDOMTarget = (type, counter) => {
 
         const colorMapping =  {
             signature: 'blue',
             name: 'red',
             email: 'green'
         }
-        const containerElt = document.querySelector(container)
+
+        const containerMapping = {
+            signature: '.sidebar-signature',
+            name: '.sidebar-name',
+            email: '.sidebar-email'
+        }
+
+        const containerElt = document.querySelector(containerMapping[type])
         const targetClassname = `target-${type}-${counter}`
         const targetElt = document.createElement('div');
 
@@ -153,8 +166,6 @@ const MainComponent = () => {
 
         return targetClassname;
     }
-
-
     // const getDefaultTargets = () => {
     //     return [
     //         {
@@ -177,10 +188,10 @@ const MainComponent = () => {
     //         }]
     // }
 
-    const addTarget = (target) => {
-        targets.push(target)
-        setTargets(targets);
-    }
+    // const addTarget = (target) => {
+    //     targets.push(target)
+    //     setTargets(targets);
+    // }
 
     const getStyle = (inputName) => {
         const colorMapping =  {
@@ -199,48 +210,33 @@ const MainComponent = () => {
     }
 
     const createMoveable = (target) => {
-        // console.log("CREATE MOVEABLE", target);
-        // target.isActive = true;
 
-        //TODO Not sure if it's needed
-        // target['toto'] = 'toto';
         const targetsCopy = JSON.parse(JSON.stringify(targets));
+
         targetsCopy.forEach((targetFromState, index) => {
             if (targetFromState.id === target.id && targetFromState.type === target.type)
                 targetFromState['isActive'] = true;
         })
-        console.log('targCopy', targetsCopy);
-        console.log('targs', targets);
+
+        console.log('targetCopy AVT AJOUT', targetsCopy);
+        // setTargets(targetsCopy);
+
+        // Recupérer le compteur après incrémentation
+        const newCounterValue = incrementCounter(target.type)
+        console.log("newCounterValue", newCounterValue);
+        console.log("post return", signatureCounter);
+        const newDomTargetName = createDOMTarget(target.type, newCounterValue)
+
+        const newTarget = {
+            domElt: newDomTargetName,
+            type: target.type,
+            isActive: false,
+            id: newCounterValue
+        }
+
+        targetsCopy.push(newTarget);
+        console.log('targetCopy APRES AJOUT', targetsCopy);
         setTargets(targetsCopy);
-
-        const newDomTargetName = createDOMTarget(target.type, )
-        // setTargets(targets);
-
-        // console.log("targets with update", targets)
-        // Créér un movable = créer un élément dans le dom, (une nouvelle div avec la classe target) et un objet target
-        // dans le state
-       //  const sidebarElt = document.querySelector('.sidebar-signature');
-       //  const newDivElt = document.createElement('div')
-       //  newDivElt.classList.add('target-signature-2');
-       //  newDivElt.textContent = 'SIGNATURE';
-       //  newDivElt.style.width = "150px";
-       //  newDivElt.style.height = "30px";
-       //  newDivElt.style.backgroundColor = "blue";
-       //  newDivElt.style.color = "white";
-       //  newDivElt.style.cursor = "pointer";
-       //  sidebarElt.appendChild(newDivElt);
-       //
-       //  const newTarget = {
-       //      domElt: document.querySelector(".target-signature-2"),
-       //      type: 'signature',
-       //      isActive: false,
-       //      id: 4,
-       //  }
-       //
-       // targetsCopy.push(newTarget);
-       //  console.log("targets pre set", targets)
-
-
     }
 
     const test = () => {
@@ -249,15 +245,17 @@ const MainComponent = () => {
         // console.log("docViewerr", docViewer);
 
         // addTarget({domElt:"toto",type:'toto'})
-        const newTarget = {
-            // domElt: document.querySelector(".target-signature-2"),
-            type: 'signatursse',
-            isActive: false,
-            id: 4,
-        }
-         //targets.push(newTarget);
-        setTargets((targets) => [...targets, newTarget]);
-        console.log("les targets", targets)
+    //     const newTarget = {
+    //         // domElt: document.querySelector(".target-signature-2"),
+    //         type: 'signatursse',
+    //         isActive: false,
+    //         id: 4,
+    //     }
+    //      //targets.push(newTarget);
+    //     setTargets((targets) => [...targets, newTarget]);
+    //     console.log("les targets", targets)
+        console.log("Liste targets", targets);
+        console.log("Liste moveAble", document.querySelectorAll('.moveable-control-box'))
     }
 
     return (
@@ -268,13 +266,21 @@ const MainComponent = () => {
                 {/*Soit on récupere la position initial de chaque target */}
                 {/*Soit on créer une div wrapper pour chaque type de target et quand on récréé une target*/}
                 {/*on l'ajoute en enfant de cette div wrapper.*/}
-                {/*<div className="sidebar-signature">*/}
-                <div className={`target-signature-${signatureCounter}`} style={getStyle('signature')}>SIGNATURE</div>
+                <div className="sidebar-signature">
+                    <div className={`target-signature-1`} style={getStyle('signature')}>SIGNATURE</div>
+                </div>
+                <div className="sidebar-name">
+                    <div className={`target-name-1`} style={getStyle('name')}>NAME</div>
+                </div>
+                <div className="sidebar-email">
+                    <div className={`target-email-1`} style={getStyle('email')}>EMAIL</div>
+                </div>
+
                 {/*/!*<div style={{marginBottom: 20}} />*!/*/}
                 {/*</div>*/}
-                <div className={`target-name-${nameCounter}`} style={getStyle('name')}>NOM</div>
+
                 {/*/!*<div style={{marginBottom: 20}}/>*!/*/}
-                <div className={`target-email-${emailCounter}`} style={getStyle('email')}>EMAIL</div>
+
                 {/*/!*<div style={{marginBottom: 20}}/>*!/*/}
                 {/*{targets.map(target => (*/}
                 {/*    <div className={`target-${target.type}-${target.id}`}*/}
@@ -292,42 +298,42 @@ const MainComponent = () => {
             {/*<div className="header">React sample</div>*/}
             <div className="webviewer" ref={viewer} style={{height: "100vh"}}></div>
         </div>
+            {/* On créer un Moveable par target*/}
             {targets.map(targetObj => {
-                console.log(`DOM.target-${targetObj.type}-${targetObj.id}`, document.querySelector(`.target-${targetObj.type}-${targetObj.id}`))
+                // console.log(`DOM.target-${targetObj.type}-${targetObj.id}`, document.querySelector(`.target-${targetObj.type}-${targetObj.id}`))
                 return (
                     <>
-                    <p>{targetObj.type}</p>
-                    <Moveable
-                        target={document.querySelector(`.target-${targetObj.type}-${targetObj.id}`)}
-                        container={null}
-                        origin={false}
-                        edge={false}
-                        draggable={true}
-                        throttleDrag={0}
-                        onDragStart={({ target, clientX, clientY }) => {
-                            console.log("onDragStart", target);
-                        }}
-                        onDrag={({
-                                     target,
-                                     beforeDelta, beforeDist,
-                                     left, top,
-                                     right, bottom,
-                                     delta, dist,
-                                     transform,
-                                     clientX, clientY,
-                                 }) => {
-                            // console.log("onDrag left, top", left, top);
-                            // target!.style.left = `${left}px`; COM BY TUTO
-                            // target!.style.top = `${top}px`; COM BY TUTO
-                            // console.log("onDrag translate", dist);
-                            target.style.transform = transform;
-                            console.log("targetOBJ", targetObj)
-                            if (!targetObj.isActive) createMoveable(targetObj)
-                        }}
-                        onDragEnd={({ target, isDrag, clientX, clientY }) => {
-                            // console.log("onDragEnd", target, isDrag);
-                        }}
-                    />
+                        <Moveable
+                            target={document.querySelector(`.target-${targetObj.type}-${targetObj.id}`)}
+                            container={null}
+                            origin={false}
+                            edge={false}
+                            draggable={true}
+                            throttleDrag={0}
+                            onDragStart={({ target, clientX, clientY }) => {
+                                console.log("onDragStart", target);
+                            }}
+                            onDrag={({
+                                         target,
+                                         beforeDelta, beforeDist,
+                                         left, top,
+                                         right, bottom,
+                                         delta, dist,
+                                         transform,
+                                         clientX, clientY,
+                                     }) => {
+                                // console.log("onDrag left, top", left, top);
+                                // target!.style.left = `${left}px`; COM BY TUTO
+                                // target!.style.top = `${top}px`; COM BY TUTO
+                                // console.log("onDrag translate", dist);
+                                target.style.transform = transform;
+                                // console.log("targetOBJ", targetObj)
+                                if (!targetObj.isActive) createMoveable(targetObj)
+                            }}
+                            onDragEnd={({ target, isDrag, clientX, clientY }) => {
+                                // console.log("onDragEnd", target, isDrag);
+                            }}
+                        />
                     </>
                 )
             })}
